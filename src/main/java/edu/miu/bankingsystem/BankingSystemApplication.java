78a041a9884c7b58c6bad67314930b17ad360282
Tsigab <107885237+Tsigab1996@users.noreply.github.com>
2023-02-14 20:15:04 -0600

    First skeleton of banking system

---
+++ src/main/java/edu/miu/bankingsystem/BankingSystemApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.bankingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/miu/bankingsystem/BankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.bankingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
