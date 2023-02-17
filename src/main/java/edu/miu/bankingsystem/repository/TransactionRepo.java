26f1cfabd1cec92909cce4a3a12b9ee8ff2d142f
Tsigab <107885237+Tsigab1996@users.noreply.github.com>
2023-02-17 15:04:28 -0600

    The viewSavingOrChecking balance, deposit and withdrawal methods are implemented

--- src/main/java/edu/miu/bankingsystem/repository/TransactionRepo.java
+++ src/main/java/edu/miu/bankingsystem/repository/TransactionRepo.java
@@ -3,7 +3,6 @@ package edu.miu.bankingsystem.repository;
 import edu.miu.bankingsystem.domian.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 
@@ -12,11 +11,11 @@ import java.util.List;
 @Repository
 public interface TransactionRepo extends JpaRepository<Transaction, Long> {
     //This is just to find the transactions using an account Id only
-    public List<Transaction> findAllByAccount_Id(long id);
-
-    //This is also JPQL to find the transactions using an account Id only
-    @Query("select t from Transaction t join t.account acct where acct.id=:id")
-    public List<Transaction> getAllTransactions(long id);
+//    public List<Transaction> findAllByAccount_Id(long id);
+//
+//    //This is also JPQL to find the transactions using an account Id only
+//    @Query("select t from Transaction t join t.account acct where acct.id=:id")
+//    public List<Transaction> getAllTransactions(long id);
 
     //This is a jpql to find the transactions of a specific customer
     @Query("select t from Transaction t join t.account acct join acct.users u where u.id=:a and acct.id=:b ")
